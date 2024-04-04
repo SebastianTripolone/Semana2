f242d46af05ac4116c68396d0aebd9839696e8e4
SebastianTripolone <seba99vte@gmail.com>
2024-04-04 16:10:05 -0300

    Repositorio Semana 2

---
+++ Semana2/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Semana2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/ Reemplazar el método index:
+
+        public string Index()
+        {
+            return "Esta es mi accion por defecto...";
+        }
+
+        //
+        //GET: /HelloWorld/Welcome/ Agregar este método:
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, Bienvenido , Acabas de entrar: {numTimes} Vez");
+        }
+    }
+}
